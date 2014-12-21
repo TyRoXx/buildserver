54fa89b46b505d3796597617f62d0fb233cae187
TyRoXx <tyroxxxx@gmail.com>
2014-12-21 15:48:43 +0100

    moved erased_observer to silicium

--- examples/build_on_push.cpp
+++ examples/build_on_push.cpp
@@ -8,6 +8,7 @@
 #include <silicium/asio/posting_observable.hpp>
 #include <silicium/observable/spawn_coroutine.hpp>
 #include <silicium/observable/spawn_observable.hpp>
+#include <silicium/observable/erased_observer.hpp>
 #include <silicium/observable/total_consumer.hpp>
 #include <silicium/observable/thread.hpp>
 #include <silicium/sink/iterator_sink.hpp>
@@ -253,49 +254,6 @@ namespace
 
 		return build_result::success;
 	}
-}
-
-namespace Si
-{
-	template <class Element>
-	struct erased_observer
-	{
-		typedef Element element_type;
-
-		erased_observer() BOOST_NOEXCEPT
-		{
-		}
-
-		template <class Observer>
-		explicit erased_observer(Observer &&observer)
-			: m_original(Si::to_unique(Si::virtualize_observer(std::forward<Observer>(observer))))
-		{
-		}
-
-		erased_observer(erased_observer &&) BOOST_NOEXCEPT = default;
-		erased_observer &operator = (erased_observer &&) BOOST_NOEXCEPT = default;
-
-		void got_element(Element element)
-		{
-			assert(m_original);
-			m_original->got_element(std::move(element));
-		}
-
-		void ended()
-		{
-			assert(m_original);
-			m_original->ended();
-		}
-
-		observer<Element> *get() const BOOST_NOEXCEPT
-		{
-			return m_original.get();
-		}
-
-	private:
-
-		std::unique_ptr<observer<Element>> m_original;
-	};
 }
 
 int main(int argc, char **argv)
