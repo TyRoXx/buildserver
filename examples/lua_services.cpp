07931bd8f08293f0babffebd0e3b0b7a7e983b22
TyRoXx <tyroxxxx@gmail.com>
2015-01-13 19:37:08 +0100

    ideas for the actual build step APIs

--- examples/lua_services.cpp
+++ examples/lua_services.cpp
@@ -5,33 +5,52 @@
 
 namespace
 {
+	void run_experiment()
+	{
+		auto lua_state = lua::create_lua();
+		lua::stack_value script = lua::load_file(*lua_state, boost::filesystem::path(__FILE__).parent_path() / "lua_services.lua").value();
+		script.release();
+		lua::stack_array script_main = lua::pcall(*lua_state, 0, 1);
+		if (script_main.size() != 1)
+		{
+			return;
+		}
+		lua::stack s(*lua_state);
+		lua::stack_value require = lua::register_any_function(s, [](Si::memory_range name, Si::memory_range version, lua_State &stack) -> lua::stack_value
+		{
+			if (boost::range::equal(name, Si::make_c_str_range("steps")) &&
+			    boost::range::equal(version, Si::make_c_str_range("1.0")))
+			{
+				lua::stack_value module = lua::create_table(stack);
+				lua::stack s(stack);
+				module.set("custom", lua::register_any_function(s, [](Si::memory_range name, lua::any_local start)
+				{
+					std::cerr << std::string(name.begin(), name.end()) << '\n';
+				}));
+				return module;
+			}
+			return lua::push_nil(stack);
+		});
+		script_main.release();
+		require.release();
+		lua::stack_array main_results = lua::pcall(*lua_state, 1, 1);
 
+	}
 }
 
 int main()
 {
 	boost::asio::io_service io;
 
-	auto lua_state = lua::create_lua();
-	lua::stack_value script = lua::load_file(*lua_state, boost::filesystem::path(__FILE__).parent_path() / "lua_services.lua").value();
-	script.release();
-	lua::stack_array script_main = lua::pcall(*lua_state, 0, 1);
-	if (script_main.size() != 1)
+	try
 	{
+		run_experiment();
+	}
+	catch (std::exception const &ex)
+	{
+		std::cerr << ex.what() << '\n';
 		return 1;
 	}
-	lua::stack s(*lua_state);
-	lua::stack_value require = lua::register_any_function(s, [](Si::memory_range name, Si::memory_range version)
-	{
-		if (boost::range::equal(name, Si::make_c_str_range("steps")) &&
-		    boost::range::equal(version, Si::make_c_str_range("1.0")))
-		{
-
-		}
-	});
-	script_main.release();
-	require.release();
-	lua::stack_array main_results = lua::pcall(*lua_state, 1, 1);
 
 	io.run();
 }
--- examples/lua_services.lua
+++ examples/lua_services.lua
@@ -1,4 +1,24 @@
 return function (require)
-	local test = require("test", "1.0")
-	return test
+	local steps = require("steps", "1.0")
+	local trigger = require("trigger", "1.0")
+	local git = require("git", "1.0")
+	local history = require("history", "1.0")
+	local change_triggered = trigger.private_web_trigger("a push happened on the repository")
+	local regular_check = trigger.timeout(change_triggered, 3600, function()
+		return true
+	end)
+	local top_message = steps.custom("download the latest commit message", function (input)
+		return git.get_commit_message({"https://github.com/TyRoXx/buildserver.git", "master"})
+	end)
+	local last_message = nil
+	local changed_messages = steps.filter("the latest commit message changed", function (current_message)
+		if last_message == current_message then
+			return false
+		end
+		last_message = current_message
+		return true
+	end)
+	local messages = regular_check:into(top_message:into(changed_messages))
+	local message_archive = history.sequence("the resulting commit messages"):get_push_step()
+	return messages:into(message_archive)
 end
