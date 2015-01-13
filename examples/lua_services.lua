43b9a29852310f318366fa0670a6a6ada60c9218
TyRoXx <tyroxxxx@gmail.com>
2015-01-13 20:55:27 +0100

    separate the description and unique ID

--- examples/lua_services.lua
+++ examples/lua_services.lua
@@ -6,7 +6,7 @@ return function (require)
 
 	-- There is a commit hook that has to be configured in detail by the
 	-- administrator.
-	local change_triggered = trigger.private_web_trigger(
+	local change_triggered = steps.describe(
 		-- The description can be changed at any time.
 		"a push happened on the repository",
 		-- This is the unique identifier (relative to this script) of
@@ -15,44 +15,46 @@ return function (require)
 		-- step and still keep its history if you keep the ID.
 		-- You can also change relationships between steps at any time.
 		"100",
-		-- Optional ID of the web trigger. The default would be the
-		-- unique identifier. This ID has to be unique among all the
-		-- web triggers in this script. 
-		"repository-pushed"
+		trigger.private_web_trigger(
+			-- Optional ID of the web trigger. The default would be the
+			-- unique identifier. This ID has to be unique among all the
+			-- web triggers in this script. 
+			"repository-pushed"
+		)
 	);
 
 	-- Trigger the build one hour after the last commit hook trigger
 	-- because that is better than nothing when the hook breaks somehow.
-	local regular_check = trigger.timeout(
+	local regular_check = steps.describe(
 		"when there is no push for an hour",
 		"200",
-		change_triggered,
-		3600,
-		function()
-			return true
-		end
-	)
+		trigger.timeout(
+			change_triggered,
+			3600,
+			function() return true end
+		)
+	);
 
 	-- Returns the latest commit message on the master branch.
 	-- Does not take any input other than data from github.com.
-	local top_message = steps.map(
+	local top_message = steps.describe(
 		"download the latest commit message",
 		"300",
-		function ()
+		steps.map(function ()
 			return git.get_commit_message({"https://github.com/TyRoXx/buildserver.git", "master"})
-		end
-	)
+		end)
+	);
 
 	-- Remembers the last message that was downloaded and discards any
 	-- messages that are equal to the previous one.
 	-- You may want to do that for example when you poll remote state.
-	local changed_messages = history.persistent_filter(
+	local changed_messages = steps.describe(
 		"the latest commit message changed",
 		"400",
-		function (current_message, last_message)
+		history.persistent_filter(function (current_message, last_message)
 			return (last_message ~= current_message), current_message
-		end
-	)
+		end)
+	);
 
 	return steps.sequence {
 		-- the first element can be either a trigger or an existing step
